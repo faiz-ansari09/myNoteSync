58c3df66a0dbb5aa2cc419d4dc7230929b63873b
Faiz <ansarifaiz0905@gmail.com>
2025-07-27 00:41:49 +0530

    refactor: reorganize index.js and add vercel configuration

--- api/index.js
+++ api/index.js
@@ -1,17 +1,28 @@
-require("dotenv").config(); // Load environment variables from .env
-const connectToMongo = require("./db");
 const express = require("express");
-const cors = require('cors');
+const connectToMongo = require("./db");
+const cors = require("cors");
+require("dotenv").config(); // Load environment variables from .env
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-connectToMongo();
+// Configure CORS
+app.use(
+  cors({
+    // origin: ['https://logi-tracker-ihzh.vercel.app'],
+    origin: ["http://localhost:3000"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Allow credentials if needed
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to myNoteSync</h1>");
+});
 
 // Available Routes
 app.use("/api/auth", require("./routes/auth"));
@@ -19,5 +30,6 @@ app.use("/api/notes", require("./routes/notes"));
 
 // Start Server
 app.listen(port, () => {
+  connectToMongo();
   console.log(`myNoteSync backend listening at http://localhost:${port}`);
 });
